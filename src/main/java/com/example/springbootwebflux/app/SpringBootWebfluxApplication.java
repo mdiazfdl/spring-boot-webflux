a1ace8882822d87b5060c27bbda32271c6edd367
mfernandez <mfernandez@soaint.com>
2023-06-05 18:32:51 -0400

    V32. Eliminando datos de una coleccion Mongo

--- src/main/java/com/example/springbootwebflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/example/springbootwebflux/app/SpringBootWebfluxApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
@@ -19,15 +20,18 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 
     @Autowired
     private ProductoDao productoDao;
-
+    @Autowired
+    private ReactiveMongoTemplate mongoTemplate;
     private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+
     @Override
     public void run(String... args) {
+        mongoTemplate.dropCollection("productos").subscribe();
         Flux.just(new Producto("mesa", 623.5),
-                new Producto("silla", 223.5),
-                new Producto("lapiz", 234.5),
-                new Producto("vaso", 213.5)
-        )
+                        new Producto("silla", 223.5),
+                        new Producto("lapiz", 234.5),
+                        new Producto("vaso", 213.5)
+                )
                 .flatMap(producto -> productoDao.save(producto))
                 .subscribe(producto -> log.info("Insert: " + producto.getId() + " nombre: " + producto.getNombre()));
     }
