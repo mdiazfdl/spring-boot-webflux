570420f59183574aee6d868227dbb09411e945f8
mfernandez <mfernandez@soaint.com>
2023-06-05 18:25:47 -0400

    V31. Insertando productos

--- src/main/java/com/example/springbootwebflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/example/springbootwebflux/app/SpringBootWebfluxApplication.java
@@ -1,13 +1,34 @@
 package com.example.springbootwebflux.app;
 
+import com.example.springbootwebflux.app.models.dao.ProductoDao;
+import com.example.springbootwebflux.app.models.documents.Producto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootWebfluxApplication {
+public class SpringBootWebfluxApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootWebfluxApplication.class, args);
     }
 
+    @Autowired
+    private ProductoDao productoDao;
+
+    private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
+    @Override
+    public void run(String... args) {
+        Flux.just(new Producto("mesa", 623.5),
+                new Producto("silla", 223.5),
+                new Producto("lapiz", 234.5),
+                new Producto("vaso", 213.5)
+        )
+                .flatMap(producto -> productoDao.save(producto))
+                .subscribe(producto -> log.info("Insert: " + producto.getId() + " nombre: " + producto.getNombre()));
+    }
 }
--- src/main/java/com/example/springbootwebflux/app/models/documents/Producto.java
+++ src/main/java/com/example/springbootwebflux/app/models/documents/Producto.java
@@ -1,18 +1,27 @@
 package com.example.springbootwebflux.app.models.documents;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Data
+@NoArgsConstructor
 @Document(collection = "productos")
 public class Producto {
     @Id
     private String id;
 
     private String nombre;
+
     private Double precio;
+
     private Date createAt;
+
+    public Producto(String nombre, Double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
 }
